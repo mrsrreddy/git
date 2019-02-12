3cadb2e35aba3cf0c289233567f8d05ce775c051
RAMYA REDDY <malluramyareddy@gmail.com>
2019-02-12 16:46:38 +0530

    aftermerging

--- a.sh
+++ a.sh
@@ -5,10 +5,9 @@ goodmorning
 task1 first line in git
 today we are practicing git
 
-task1 first line in git 
+sk1 first line in git 
 edit from remote repository
-refs/remotes/origin/master
-afhsdgf<F4>dg
+fhsdgf<F4>dg
 A
 B
 C
